315b31571705b93114291d5cf73de427e0ae32c8
HassanAliJunejo <160749752+HassanAliJunejo@users.noreply.github.com>
2025-01-28 10:52:16 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Brands from "@/components/Brands";
+import HappyCustomers from "@/components/HappyCustomers";
+import Hero from "@/components/Hero";
+import NewArrivals from "@/components/NewArrivals";
+import Styles from "@/components/Styles";
+import TopSelling from "@/components/TopSelling";
+
+export default function Home() {
+  return (
+    <>
+    <Hero/>
+    <Brands/>
+    <NewArrivals/>
+    <TopSelling/>
+    <Styles/>
+    <HappyCustomers/>
+
+    </>
+  );
+}
